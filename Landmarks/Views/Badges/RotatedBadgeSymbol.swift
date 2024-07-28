2bb403bc04e999ad977112610d75081478441606
Eason Chang <eason@easonchang.com>
2024-07-28 13:34:02 -0600

    Animation

---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,22 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Eason on 2024/7/26.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol(angle: Angle(degrees: 5))
+}
---
+++ Landmarks/Views/Hike.swift
@@ -0,0 +1,18 @@
+//
+//  Hike.swift
+//  Landmarks
+//
+//  Created by Eason on 2024/7/28.
+//
+
+import SwiftUI
+
+struct Hike: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Hike()
+}
